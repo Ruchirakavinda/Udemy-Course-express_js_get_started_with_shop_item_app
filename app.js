563408b19b3e364338e036138500996ccbc92f5b
Ruchirakavinda <ruchirakvnd@gmail.com>
2021-07-13 01:13:37 +0530

    Generate dynamic content with ejs

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 
 const app = express();
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.listen(3000);
 
 app.get('/',(req,res)=>{
-    res.render("index")
+
+    const items = [
+        {name:"Book Bundle",price:"1.05"},
+        {name:"Computer",price:"100.0"},
+        {name:"Mobile Phone",price:"70.5"},
+        {name:"Study Table",price:"50.0"}
+    ]
+    
+    res.render("index",{ items})
 });
 
 app.get('/add_item',(req,res)=>{
